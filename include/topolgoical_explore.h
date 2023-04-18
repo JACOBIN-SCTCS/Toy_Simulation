f080fc003b209fbb71c2fabe46ef2fd638018017
JACOBIN-SCTCS <jacobja3@gmail.com>
2023-04-18 04:59:15 +0000

    Topological explore

--- include/topolgoical_explore.h
+++ include/topolgoical_explore.h
@@ -25,15 +25,15 @@ class TopolgicalExplore
             std::vector<std::pair<int,int>> cells;
         };
         
-        struct DijkstraNode
+        struct AstarNode
         {
             std::complex<double> point;
             Eigen::VectorXd h_signature;
             double cost;
-            struct DijkstraNode* parent; 
+            struct AstarNode* parent; 
             std::vector<std::complex<double>> edge;
 
-            DijkstraNode( std::complex<double> p , Eigen::VectorXd h , double c , struct DijkstraNode* pa ,std::vector<std::complex<double>> e) : point(p) , h_signature(h),cost(c),parent(pa) , edge(e) {}
+            AstarNode( std::complex<double> p , Eigen::VectorXd h , double c , struct AstarNode* pa ,std::vector<std::complex<double>> e) : point(p) , h_signature(h),cost(c),parent(pa) , edge(e) {}
         };
         
         TopolgicalExplore(std::vector<std::vector<int>>* g , std::vector<std::vector<int>> * o) : grid(g) , obstacles_seen(o)
@@ -59,101 +59,101 @@ class TopolgicalExplore
             return neighbours;
         }
 
-        void findFrontiers(int x, int y)
-        {
-            frontiers.clear();
-            std::vector<std::vector<int>>& grid_ref = *grid;
-            bool visited[grid_ref.size()][grid_ref[0].size()];
-            bool frontier_tag[grid_ref.size()][grid_ref[0].size()];
-            std::queue<std::pair<int,int>> q;
-            q.push({x,y});
-            visited[x][y] = true;
-            while(!q.empty())
-            {
-                std::pair<int,int> curr  = q.front();
-                q.pop();
-                int i = curr.first;
-                int j = curr.second;
-                std::vector<std::pair<int,int>> neighbours = getNeighbours(i,j,grid_ref.size(),grid_ref[0].size());
-                for(int k=0;k<neighbours.size();++k)
-                {
-                    int x_new = neighbours[k].first;
-                    int y_new = neighbours[k].second;
-                    if(visited[x_new][y_new] == true || frontier_tag[x_new][y_new]==true)
-                        continue;
-                    visited[x_new][y_new] = true;
+        // void findFrontiers(int x, int y)
+        // {
+        //     frontiers.clear();
+        //     std::vector<std::vector<int>>& grid_ref = *grid;
+        //     bool visited[grid_ref.size()][grid_ref[0].size()];
+        //     bool frontier_tag[grid_ref.size()][grid_ref[0].size()];
+        //     std::queue<std::pair<int,int>> q;
+        //     q.push({x,y});
+        //     visited[x][y] = true;
+        //     while(!q.empty())
+        //     {
+        //         std::pair<int,int> curr  = q.front();
+        //         q.pop();
+        //         int i = curr.first;
+        //         int j = curr.second;
+        //         std::vector<std::pair<int,int>> neighbours = getNeighbours(i,j,grid_ref.size(),grid_ref[0].size());
+        //         for(int k=0;k<neighbours.size();++k)
+        //         {
+        //             int x_new = neighbours[k].first;
+        //             int y_new = neighbours[k].second;
+        //             if(visited[x_new][y_new] == true || frontier_tag[x_new][y_new]==true)
+        //                 continue;
+        //             visited[x_new][y_new] = true;
                     
-                    if(grid_ref[x_new][y_new]==0)
-                        continue;
+        //             if(grid_ref[x_new][y_new]==0)
+        //                 continue;
                     
-                    else if(grid_ref[x_new][y_new]==-1)
-                    {
-                        std::vector<std::pair<int,int>> cell_neighbours = getNeighbours(x_new,y_new,grid_ref.size(),grid_ref[0].size());
-                        for(int j =0;j<4;++j)
-                        {
-                            int x_prime_new = cell_neighbours[j].first;
-                            int y_prime_new = cell_neighbours[j].second;
-                            if(grid_ref[x_prime_new][y_prime_new]==1)
-                            {
-                                frontier_tag[x_new][y_new] = true;
-                                break;
-                            }
-                        }
-                        if(frontier_tag[x_new][y_new]==true)
-                        {
-                            std::queue<std::pair<int,int>> new_q;
-                            new_q.push({x_new,y_new});
-                            std::vector<std::pair<int,int>> frontier_cells;
-                            frontier_cells.push_back({x_new,y_new});
-                            while(!new_q.empty())
-                            {
-                                std::vector<std::pair<int,int>> cell_neighbors = getNeighbours(new_q.front().first,new_q.front().second,grid_ref.size(),grid_ref[0].size());
-                                new_q.pop();
-                                for(int j=0;j<cell_neighbors.size();++j)
-                                {
-                                    int x_prime_new = cell_neighbors[j].first;
-                                    int y_prime_new = cell_neighbors[j].second;
-                                    if(visited[x_prime_new][y_prime_new]==true || frontier_tag[x_prime_new][y_prime_new]==true)
-                                        continue;
-                                    visited[x_prime_new][y_prime_new] = true;
+        //             else if(grid_ref[x_new][y_new]==-1)
+        //             {
+        //                 std::vector<std::pair<int,int>> cell_neighbours = getNeighbours(x_new,y_new,grid_ref.size(),grid_ref[0].size());
+        //                 for(int j =0;j<4;++j)
+        //                 {
+        //                     int x_prime_new = cell_neighbours[j].first;
+        //                     int y_prime_new = cell_neighbours[j].second;
+        //                     if(grid_ref[x_prime_new][y_prime_new]==1)
+        //                     {
+        //                         frontier_tag[x_new][y_new] = true;
+        //                         break;
+        //                     }
+        //                 }
+        //                 if(frontier_tag[x_new][y_new]==true)
+        //                 {
+        //                     std::queue<std::pair<int,int>> new_q;
+        //                     new_q.push({x_new,y_new});
+        //                     std::vector<std::pair<int,int>> frontier_cells;
+        //                     frontier_cells.push_back({x_new,y_new});
+        //                     while(!new_q.empty())
+        //                     {
+        //                         std::vector<std::pair<int,int>> cell_neighbors = getNeighbours(new_q.front().first,new_q.front().second,grid_ref.size(),grid_ref[0].size());
+        //                         new_q.pop();
+        //                         for(int j=0;j<cell_neighbors.size();++j)
+        //                         {
+        //                             int x_prime_new = cell_neighbors[j].first;
+        //                             int y_prime_new = cell_neighbors[j].second;
+        //                             if(visited[x_prime_new][y_prime_new]==true || frontier_tag[x_prime_new][y_prime_new]==true)
+        //                                 continue;
+        //                             visited[x_prime_new][y_prime_new] = true;
                                 
-                                    if(grid_ref[x_prime_new][y_prime_new]==-1)
-                                    {
-                                        std::vector<std::pair<int,int>> frontier_cell_neighbours = getNeighbours(x_prime_new,y_prime_new,grid_ref.size(),grid_ref[0].size());  
-                                        for(int k=0;k<4;++k)
-                                        {
-                                            int x_prime_prime_new = frontier_cell_neighbours[k].first;
-                                            int y_prime_prime_new = frontier_cell_neighbours[k].second;
-                                            if(grid_ref[x_prime_prime_new][y_prime_prime_new]==1)
-                                            {
-                                                frontier_tag[x_prime_new][y_prime_new] = true;
-                                                break;
-                                            }
-                                        }
-                                        if(frontier_tag[x_prime_new][y_prime_new]==true)
-                                        {
-                                            new_q.push({x_prime_new,y_prime_new});
-                                            frontier_cells.push_back({x_prime_new,y_prime_new});
-                                        }                                      
-                                    }
-                                }
-                            } 
-                            Frontier frontier;
-                            frontier.x = x_new;
-                            frontier.y = y_new;
-                            frontier.cells = frontier_cells;
-                            frontiers.push_back(frontier);
-                        }
-                    }
-                    else
-                    {
-                        q.push({x_new,y_new});
+        //                             if(grid_ref[x_prime_new][y_prime_new]==-1)
+        //                             {
+        //                                 std::vector<std::pair<int,int>> frontier_cell_neighbours = getNeighbours(x_prime_new,y_prime_new,grid_ref.size(),grid_ref[0].size());  
+        //                                 for(int k=0;k<4;++k)
+        //                                 {
+        //                                     int x_prime_prime_new = frontier_cell_neighbours[k].first;
+        //                                     int y_prime_prime_new = frontier_cell_neighbours[k].second;
+        //                                     if(grid_ref[x_prime_prime_new][y_prime_prime_new]==1)
+        //                                     {
+        //                                         frontier_tag[x_prime_new][y_prime_new] = true;
+        //                                         break;
+        //                                     }
+        //                                 }
+        //                                 if(frontier_tag[x_prime_new][y_prime_new]==true)
+        //                                 {
+        //                                     new_q.push({x_prime_new,y_prime_new});
+        //                                     frontier_cells.push_back({x_prime_new,y_prime_new});
+        //                                 }                                      
+        //                             }
+        //                         }
+        //                     } 
+        //                     Frontier frontier;
+        //                     frontier.x = x_new;
+        //                     frontier.y = y_new;
+        //                     frontier.cells = frontier_cells;
+        //                     frontiers.push_back(frontier);
+        //                 }
+        //             }
+        //             else
+        //             {
+        //                 q.push({x_new,y_new});
                    
-                    }
-                }
+        //             }
+        //         }
 
-            }
-        }
+        //     }
+        // }
         
 
 
@@ -205,7 +205,7 @@ class TopolgicalExplore
 
 
             std::vector<std::vector<std::pair<int,int>>> paths;	
-            std::priority_queue<DijkstraNode*, std::vector<DijkstraNode*>, std::function<bool(DijkstraNode*, DijkstraNode*)>> pq([](DijkstraNode* a, DijkstraNode* b) { return a->cost > b->cost; });
+            std::priority_queue<AstarNode*, std::vector<AstarNode*>, std::function<bool(AstarNode*,AstarNode*)>> pq([](AstarNode* a, AstarNode* b) { return a->cost > b->cost; });
             std::unordered_map<std::string,double> distance_count;
             std::set<std::string> visited;
             std::stringstream ss;
@@ -229,13 +229,13 @@ class TopolgicalExplore
                     cell_cost = 1.0;
                 double c = cell_cost + std::abs(new_point-goal_point);
                 std::vector<std::complex<double>> e = {start_point,new_point};
-                DijkstraNode* node = new DijkstraNode(new_point,temp,c,NULL,e);
+                AstarNode* node = new AstarNode(new_point,temp,c,NULL,e);
                 pq.push(node);
 	        }
 	
             while(!pq.empty())
             {
-                DijkstraNode* node = pq.top();
+                AstarNode* node = pq.top();
                 pq.pop();
                 if(node->point == goal_point)
                 {
@@ -253,7 +253,7 @@ class TopolgicalExplore
                         count +=1;
                         visited.insert(key);
                         std::vector<std::pair<int,int>> path;
-                        DijkstraNode* temp = node;
+                        AstarNode* temp = node;
                         while(temp!=NULL)
                         {
                             int current_point_x  = temp->point.real(), current_point_y = temp->point.imag();
@@ -294,7 +294,7 @@ class TopolgicalExplore
                         {
                             distance_count[key] = c;
                             std::vector<std::complex<double>> edge = { node->point,new_point}; 
-                            DijkstraNode* new_node = new DijkstraNode(new_point,temp,c,node,edge);
+                            AstarNode* new_node = new AstarNode(new_point,temp,c,node,edge);
                             pq.push(new_node);
                         }
                     }
--- src/main.cpp
+++ src/main.cpp
@@ -121,40 +121,6 @@ public:
 			fw.render_screen(grid);
 			f_explore.findFrontiers(current_x, current_y);
 		}
-		
-		// for(int i= 0;i < 30;++i)
-		// {
-		// 	sensor_model(x+i,y+i);
-		// 	grid[x+i][y+i] = 2;
-		// 	fw.render_screen(grid);
-		// }
-
-		// FrontierExplore f_explore(&grid);
-		// f_explore.findFrontiers(x,y);
-		// std::cout<<"Before computing path"<<std::endl;
-		// std::vector<std::pair<int,int>> path = f_explore.getPath(x+29,y+29,f_explore.frontiers[0].cells[0].first,f_explore.frontiers[0].cells[0].second);
-		// // std::cout<<"After computing path between source and destination node"<<std::endl;
-		// // std::cout << "Length of path = " <<path.size()<<std::endl;
-		
-		// for(int i=0;i<path.size();++i)
-		// {
-		// 	grid[path[i].first][path[i].second] = 4;
-		// }
-		// grid[x][y] = 2;
-		// grid[f_explore.frontiers[0].x][f_explore.frontiers[0].y] = 3;
-
-		// for(int i =0;i<f_explore.frontiers[0].cells.size();++i)
-		// {
-		// 		grid[f_explore.frontiers[0].cells[i].first][f_explore.frontiers[0].cells[i].second] = 2;
-		// }
-		// std::cout << "Path size = " << path.size() << std::endl;
-		// for(int i=0; i<path.size();++i)
-		// {
-		// 	grid[path[i].first][path[i].second] = 2;
-		// 	fw.render_screen(grid);
-		// }
-		// fw.render_screen(grid);
-
 	}
 
 	void topological_explore(int x , int y)
@@ -172,162 +138,10 @@ public:
 		}
 		sensor_model(current_x, current_y);
 		TopolgicalExplore top_explore(&grid, &obstacles_seen);
-		top_explore.findFrontiers(current_x, current_y);
-		int previous_obstacle_count = obstacles_seen.size();
-	
-		while (top_explore.frontiers.size() > 0)
-		{
-			std::cout<<"Finding paths";
-			std::vector<std::vector<std::pair<int,int>>> paths = top_explore.getPaths(current_x, current_y);
-			std::cout<<"Paths size = "<<paths.size()<<std::endl;
-			
-			while(paths.size() >0)
-			{
-				bool followed_path = true;
-				for(int i=0;i<paths[0].size();++i)
-					grid[paths[0][i].first][paths[0][i].second] = 3;
-				
-				for (int i = 0; i < paths[0].size(); ++i)
-				{
-					grid[current_x][current_y] = 1;
-					if(grid[paths[0][i].first][paths[0][i].second]==0)
-					{
-						int j = i;
-						while(j < paths[0].size() && grid[paths[0][j].first][paths[0][j].second]!=-1)
-						{
-							//grid[paths[0][j].first][paths[0][j].second] = 1;
-							if(grid[paths[0][j].first][paths[0][j].second]==0)
-							{
-								j++;
-								continue;
-							}
-							else
-							{
-								break;
-							}				
-						}
-						if(j==paths[0].size())
-						{
-							followed_path=false;
-							break;
-						}
-						else
-						{
-							std::vector<std::pair<int,int>> path = top_explore.getPath(paths[0][i].first,paths[0][i].second,paths[0][j].first,paths[0][j].second);
-							if(path.size()==0)
-							{
-								followed_path=false;
-								break;
-							}
-							for (int k = 0; k < path.size(); ++k)
-							{
-								grid[path[k].first][path[k].second] = 2;
-								sensor_model(path[k].first,path[k].second);
-								fw.render_screen(grid);
-								SDL_Delay(500);
-							}
-							i = j;
-						}	
-					}
-					current_x = paths[0][i].first;
-					current_y = paths[0][i].second;
-					grid[current_x][current_y] = 2;
-					sensor_model(current_x,current_y);
-					fw.render_screen(grid);				
-					SDL_Delay(500);
-				}
-					
-				paths.erase(paths.begin());
-				if(obstacles_seen.size() > previous_obstacle_count || !followed_path)
-				{
-					previous_obstacle_count = obstacles_seen.size();
-					break;
-				}
-				else
-				{
-					
-					if(paths.size()>0)
-					{
-						std::vector<std::pair<int,int>> path = paths[0];
-						
-						std::reverse(path.begin(),path.end());
-						for(int i=0;i<path.size();++i)
-							grid[path[i].first][path[i].second] = 3;
-
-						for (int i = 0; i < path.size(); ++i)
-						{
-							grid[current_x][current_y] = 1;
-							if(grid[path[i].first][path[i].second]==0)
-							{
-								int j = i;
-								while(j < path.size() && grid[path[j].first][path[j].second]!=-1)
-								{
-									//grid[paths[0][j].first][paths[0][j].second] = 1;
-									if(grid[path[j].first][path[j].second]==0)
-									{
-										j++;
-										continue;
-									}
-									else
-									{
-										break;
-									}				
-								}
-								if(j==path.size())
-								{
-									followed_path=false;
-									break;
-								}
-								else
-								{
-									std::vector<std::pair<int,int>> new_path = top_explore.getPath(path[i].first,path[i].second,path[j].first,path[j].second);
-									if(new_path.size()==0)
-									{
-										followed_path=false;
-										break;
-									}
-									for (int k = 0; k < new_path.size(); ++k)
-									{
-										grid[new_path[k].first][new_path[k].second] = 2;
-										sensor_model(new_path[k].first,new_path[k].second);
-										fw.render_screen(grid);
-										SDL_Delay(500);
-									}
-									i = j;
-								}	
-							}
-							current_x = path[i].first;
-							current_y = path[i].second;
-							grid[current_x][current_y] = 2;
-							sensor_model(current_x,current_y);
-							fw.render_screen(grid);				
-							SDL_Delay(500);
-						}
-						paths.erase(paths.begin());	
-					}
-				}
-			}
-			top_explore.findFrontiers(current_x, current_y);
-		}
 		
-
-
-		// for(int i=0;i<paths.size();++i)
-		// {
-		// 	std::cout<<"\n";
-		// 	for(int j=0;j<paths[i].size();++j)
-		// 	{
-		// 		std::cout<<"("<<paths[i][j].first<<","<<paths[i][j].second<<") "<<std::endl;
-
-		// 		grid[paths[i][j].first][paths[i][j].second] = 3;
-		// 		fw.render_screen(grid);
-		// 		SDL_Delay(500);
-				
-		// 	}
-		// }
-
-
 	}
+	
+	
 
 	Framework fw;
 
@@ -346,8 +160,8 @@ int main(int argc, char *argv[])
 	srand(time(NULL));
 
 	Robot robot(60, 600,10.0, 20);
-	// robot.start_exploring(10, 10);
-	robot.topological_explore(10,10);
+	robot.start_exploring(10, 10);
+	// robot.topological_explore(10,10);
 	// robot.setInitialRobotPose(5,5);
 
 	// robot.fw.draw_point(300,300);
