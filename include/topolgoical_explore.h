3afd4e89e54a8fa38729b206e3a4019a1a645385
JACOBIN-SCTCS <jacobja3@gmail.com>
2023-04-24 04:59:53 +0000

    temporary commit

--- include/topolgoical_explore.h
+++ include/topolgoical_explore.h
@@ -64,104 +64,6 @@ public:
         return neighbours;
     }
 
-    // void findFrontiers(int x, int y)
-    // {
-    //     frontiers.clear();
-    //     std::vector<std::vector<int>>& grid_ref = *grid;
-    //     bool visited[grid_ref.size()][grid_ref[0].size()];
-    //     bool frontier_tag[grid_ref.size()][grid_ref[0].size()];
-    //     std::queue<std::pair<int,int>> q;
-    //     q.push({x,y});
-    //     visited[x][y] = true;
-    //     while(!q.empty())
-    //     {
-    //         std::pair<int,int> curr  = q.front();
-    //         q.pop();
-    //         int i = curr.first;
-    //         int j = curr.second;
-    //         std::vector<std::pair<int,int>> neighbours = getNeighbours(i,j,grid_ref.size(),grid_ref[0].size());
-    //         for(int k=0;k<neighbours.size();++k)
-    //         {
-    //             int x_new = neighbours[k].first;
-    //             int y_new = neighbours[k].second;
-    //             if(visited[x_new][y_new] == true || frontier_tag[x_new][y_new]==true)
-    //                 continue;
-    //             visited[x_new][y_new] = true;
-
-    //             if(grid_ref[x_new][y_new]==0)
-    //                 continue;
-
-    //             else if(grid_ref[x_new][y_new]==-1)
-    //             {
-    //                 std::vector<std::pair<int,int>> cell_neighbours = getNeighbours(x_new,y_new,grid_ref.size(),grid_ref[0].size());
-    //                 for(int j =0;j<4;++j)
-    //                 {
-    //                     int x_prime_new = cell_neighbours[j].first;
-    //                     int y_prime_new = cell_neighbours[j].second;
-    //                     if(grid_ref[x_prime_new][y_prime_new]==1)
-    //                     {
-    //                         frontier_tag[x_new][y_new] = true;
-    //                         break;
-    //                     }
-    //                 }
-    //                 if(frontier_tag[x_new][y_new]==true)
-    //                 {
-    //                     std::queue<std::pair<int,int>> new_q;
-    //                     new_q.push({x_new,y_new});
-    //                     std::vector<std::pair<int,int>> frontier_cells;
-    //                     frontier_cells.push_back({x_new,y_new});
-    //                     while(!new_q.empty())
-    //                     {
-    //                         std::vector<std::pair<int,int>> cell_neighbors = getNeighbours(new_q.front().first,new_q.front().second,grid_ref.size(),grid_ref[0].size());
-    //                         new_q.pop();
-    //                         for(int j=0;j<cell_neighbors.size();++j)
-    //                         {
-    //                             int x_prime_new = cell_neighbors[j].first;
-    //                             int y_prime_new = cell_neighbors[j].second;
-    //                             if(visited[x_prime_new][y_prime_new]==true || frontier_tag[x_prime_new][y_prime_new]==true)
-    //                                 continue;
-    //                             visited[x_prime_new][y_prime_new] = true;
-
-    //                             if(grid_ref[x_prime_new][y_prime_new]==-1)
-    //                             {
-    //                                 std::vector<std::pair<int,int>> frontier_cell_neighbours = getNeighbours(x_prime_new,y_prime_new,grid_ref.size(),grid_ref[0].size());
-    //                                 for(int k=0;k<4;++k)
-    //                                 {
-    //                                     int x_prime_prime_new = frontier_cell_neighbours[k].first;
-    //                                     int y_prime_prime_new = frontier_cell_neighbours[k].second;
-    //                                     if(grid_ref[x_prime_prime_new][y_prime_prime_new]==1)
-    //                                     {
-    //                                         frontier_tag[x_prime_new][y_prime_new] = true;
-    //                                         break;
-    //                                     }
-    //                                 }
-    //                                 if(frontier_tag[x_prime_new][y_prime_new]==true)
-    //                                 {
-    //                                     new_q.push({x_prime_new,y_prime_new});
-    //                                     frontier_cells.push_back({x_prime_new,y_prime_new});
-    //                                 }
-    //                             }
-    //                         }
-    //                     }
-    //                     Frontier frontier;
-    //                     frontier.x = x_new;
-    //                     frontier.y = y_new;
-    //                     frontier.cells = frontier_cells;
-    //                     frontiers.push_back(frontier);
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 q.push({x_new,y_new});
-
-    //             }
-    //         }
-
-    //     }
-    // }
-
-    // std::vector<std::vector<std::pair<int,int>>> getPaths(int x , int y , int end_x, int end_y)
-
     struct PathsToFollow getPaths(int x, int y, int end_x, int end_y)
     {
         auto customOp = [](const std::complex<double> &a, const std::complex<double> &b) -> double
@@ -634,6 +536,11 @@ public:
             }
         }
         return p;
+    }
+
+    void replan()
+    {
+        
     }
 
 std::vector<std::vector<int>> *grid;
